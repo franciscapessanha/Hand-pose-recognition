780cc1abbcc5e5f7d0c2b4fbfea9301146ce9d52
Pipas_ <19pipas@gmail.com>
2018-10-30 16:22:23 +0000

    Add handlers for program arguments

--- main.py
+++ main.py
@@ -1,85 +1,17 @@
-import cv2 as cv
-import numpy as np
 import sys
-from calculate_mask import get_mask
-from display import *
+from helpers import is_int
+from video_labeling import label_video
 
-title = 'Hand Labeling v0.1'
-state = 'start'
-sample = None
-threshold = None
-
-def format_frame(frame):
-  frame = cv.flip(frame, 1)
-  frame = cv.resize(frame, (640,360)) #this way every video will have the same dimension - and so the kernels will be right!
-  return frame
-
-def enter_pressed(frame):
-  global state, threshold, sample
-  if state == 'start':
-    cv.destroyWindow(title + ' - Press ENTER to sample')
-    sample, threshold = open_selector_window(frame)
-    state = 'labeling'
-  elif state == 'calibrating':
-    cv.destroyWindow(calibrate_window_title)
-    state = 'labeling'
-
-def s_key_pressed(frame):
-  global state, threshold, sample
-  if state == 'labeling':
-    sample, threshold = open_selector_window(frame)
-
-def c_key_pressed():
-  global state
-  if state == 'labeling':
-    cv.destroyWindow(title)
-    create_calibration_window()
-    state = 'calibrating'
-
-def handle_key(key, frame):
-  if key == 27: # Esc key pressed
-    return False
-  elif key == 13: # Enter key pressed
-    enter_pressed(frame)
-  elif key == 99: # C key pressed
-    c_key_pressed()
-  elif key == 115: # S key pressed
-    s_key_pressed(frame)
-
-  return True
-
-def handle_display(frame):
-  global state
-  if state == 'start':
-    cv.imshow(title + ' - Press ENTER to sample', frame)
-  elif state == 'labeling':
-    mask_with_contours, _ = get_mask(frame, threshold)
-    cv.imshow(title, mask_with_contours)
-  elif state == 'calibrating':
-    open_calibration_window(frame, sample)
-
-def main():
-  if len(sys.argv) > 1:
-    videoCaptureDevice = int(sys.argv[1])
-  else:
-    videoCaptureDevice = 0
-
-  cap = cv.VideoCapture('hand.mp4')
-
-  while(True):
-    # Capture frame-by-frame
-    _, frame = cap.read()
-
-    frame = format_frame(frame)
-
-    if not handle_key(cv.waitKey(35), frame): 
-      break
-
-    handle_display(frame)
-
-  # When everything done, release the capture
-  cap.release()
-  cv.destroyAllWindows()
+def handle_arguments():
+  if len(sys.argv) == 1:
+    label_video(0)
+  elif len(sys.argv) == 2:
+    if is_int(sys.argv[1]):
+      label_video(int(sys.argv[1]))
+    else:
+      if sys.argv[1].endswith('.mp4'):
+        label_video(sys.argv[1])
 
 if __name__ == "__main__":
-  main()
+  handle_arguments()
+
