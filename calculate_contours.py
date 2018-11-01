13722e51098f26286ce9b4fc740bd5ec79ecd278
Francisca Pessanha <mariafranciscapessanha@gmail.com>
2018-11-01 20:16:11 +0000

    Crops hand (right side up and horizontally (right to left))

--- calculate_contours.py
+++ calculate_contours.py
@@ -5,8 +5,8 @@ from numpy import linalg
 def get_contours(mask):
   contours = find_contours(mask)
   mask = crop_mask (contours, mask)
-  contours = find_contours(mask)
-  mask = fill_contours(contours,mask)
+  #contours = find_contours(mask)
+  #mask = fill_contours(contours,mask)
   return contours
 
 def find_contours(mask):
@@ -22,7 +22,7 @@ def fill_contours(contours,mask):
     cv.fillPoly(new_mask, pts =np.asarray(contours), color=(255,255,255))
     mask = crop_mask(contours, mask)
 
-  return new_mask
+  return mask
 
 def draw_contours(frame_copy, contours):
   cv.drawContours(frame_copy, contours,-1,(0,255,0),2) # green - color for contours 
@@ -33,39 +33,76 @@ def crop_mask(contours, mask):
     x,y,w,h= cv.boundingRect(contour)
     if h > w: #image is vertical
       mask = crop_vertical_mask(mask)
-    else:
+      return mask
+    if w >= h: #image is horizontal
       mask = crop_horizontal_mask(mask)
-
-  return mask
+      return mask
+    
+##deve haver uma forma mais compacta de fazer isto 
 
 def crop_vertical_mask(mask): #fecha o contorno inferiormente
   mask_row_sum = np.sum(mask,axis=1)
-  bottom_to_top = list(reversed(mask_row_sum))
-  for i in range(0,len(bottom_to_top)-20):
-      if bottom_to_top[i+20] > 1.1*bottom_to_top[i]: #se o de cima for muito maior que o debaixo temos uma transição
-        index_in_mask = mask.shape[0]-i
-        non_zero_indexes = np.argwhere(mask[index_in_mask-1]) #o index in mask vai ser removido abaixo
-        first = non_zero_indexes[0]
-        last = non_zero_indexes[-1]
-        for x in range(np.int(first),np.int(last)):
-          mask[index_in_mask-1][x] = 255
-        
-        mask = mask[0:index_in_mask,0:mask.shape[1]]
-        return mask
+  if mask_row_sum[0] == 0: #hand is right side up
+    bottom_to_top = list(reversed(mask_row_sum))
+    for i in range(0,len(bottom_to_top)-20):
+        if bottom_to_top[i+20] > 1.1*bottom_to_top[i]: #se o de cima for muito maior que o debaixo temos uma transição
+          index_in_mask = mask.shape[0]-i
+          non_zero_indexes = np.argwhere(mask[index_in_mask-1][:]) #o index in mask vai ser removido abaixo
+          first = non_zero_indexes[0]
+          last = non_zero_indexes[-1]
+          for x in range(np.int(first),np.int(last)):
+            mask[index_in_mask-1][x] = 255
+            mask = mask[0:index_in_mask,0:mask.shape[1]]
+          return mask
+  else: #não funciona :/
+    top_to_bottom = mask_row_sum
+    for i in range(0,len(top_to_bottom)-20):
+        if top_to_bottom[i+20] > 1.1*top_to_bottom[i]: #se o de cima for muito maior que o debaixo temos uma transição
+          index_in_mask = i
+          non_zero_indexes = np.argwhere(mask[index_in_mask-1][:]) #o index in mask vai ser removido abaixo
+          first = non_zero_indexes[0]
+          last = non_zero_indexes[-1]
+          for x in range(np.int(first),np.int(last)):
+            mask[index_in_mask-1][x] = 255
+            mask = mask[index_in_mask:mask.shape[0],0:mask.shape[1]]
+            return mask
         
 def crop_horizontal_mask(mask): #fecha o contorno inferiormente
+ 
   mask_col_sum = np.sum(mask,axis=0)
-  mask_col_sum = list(reversed(mask_col_sum))
-  print(mask_col_sum)
-  for i in range(0,len(mask_col_sum)-20):
-      if mask_col_sum[i+20] > 1.1*mask_col_sum[i]: #se o de cima for muito maior que o debaixo temos uma transição
-        index_in_mask = shape[1]-i
-        non_zero_indexes = np.argwhere(mask[:][i]) #o index in mask vai ser removido abaixo
-        first = non_zero_indexes[0]
-        last = non_zero_indexes[-1]
-        for x in range(np.int(first),np.int(last)):
-          mask[y][index_in_mask-1] = 255
-        
-        mask = mask[0:mask.shape[0],0:index_in_mask]
-        return mask
+  if mask_col_sum[0] == 0:
+    right_to_left = list(reversed(mask_col_sum))
+    for i in range(0,len(right_to_left)-20):
+        if right_to_left[i+20] > 1.1*right_to_left[i]: #se o de cima for muito maior que o debaixo temos uma transição
+          index_in_mask = mask.shape[1]-i
+          non_zero_indexes = np.argwhere(np.transpose(mask[:][index_in_mask-1])) #o index in mask vai ser removido abaixo
+          first = non_zero_indexes[0]
+          last = non_zero_indexes[-1]
+          print(last)
+          for y in range(np.int(first),np.int(last)):
+            mask[y][index_in_mask-1] = 255
+            mask = mask[0:mask.shape[0],0:index_in_mask]
+            return mask
+  else: #não funciona :/
+    left_to_right = mask_col_sum 
+    for i in range(0,len(left_to_right)-20):
+        print(i)
+        if left_to_right[i+20] > 1.1*left_to_right[i]: #se o de cima for muito maior que o debaixo temos uma transição
+          index_in_mask = i
+          new_mask = np.copy(mask)
+          cv.imshow("new mask", mask)
+          non_zero_indexes = np.argwhere(np.transpose(mask[:][index_in_mask-1])) #o index in mask vai ser removido abaixo
+          print(np.transpose(mask[:][index_in_mask-1]))
+          '''
+          #first = non_zero_indexes[0]
+          #last = non_zero_indexes[-1]
+          #print(last)
+          for y in range(np.int(first),np.int(last)):
+            mask[y][index_in_mask-1] = 255
+            print(index_in_mask)
+            mask = mask[0:mask.shape[0],index_in_mask:mask.shape[1]]
+            cv.imshow("mascara",mask)
+          '''
+          return mask
+
 
